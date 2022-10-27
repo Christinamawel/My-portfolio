1456886e5f563e8ec32012f5b31fc470e68eb8ad
Christinamawel <christinamawel@gmail.com>
2022-10-27 16:50:56 -0700

    align carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,15 +5,24 @@ function Carousel(props) {
 
   const titleStyles = {
     fontSize: '2.2rem',
-    marginLeft: '5vw'  
+    marginLeft: '5vw',
+    marginBottom: '0'  
   }
 
   const arrowStyles = {
     width: '20px',
     height: '20px',
-    borderLeft: '3px solid #EFA18D',
-    borderBottom: '3px solid #EFA18D',
-    }
+    borderLeft: '5px solid #EFA18D',
+    borderBottom: '5px solid #EFA18D',
+  }
+  
+  const flexContainer = {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '2vw',
+  }
 
   const leftStyle = {
     transform: 'rotate(45deg)',
@@ -23,11 +32,30 @@ function Carousel(props) {
     transform: 'rotate(225deg)',
   }
 
+  const smCircleStyle = {
+    width: '300px',
+    height: '300px',
+    borderRadius: '50%',
+    backgroundColor: '#EFA18D'
+  }
+
+  const lgCircleStyle = {
+    width: '400px',
+    height: '400px',
+    borderRadius: '50%',
+    backgroundColor: '#EFA18D'
+  }
+
   return (
     <div>
       <p style={titleStyles}>{props.title}</p>
-      <div style={{...arrowStyles, ...leftStyle}}/>
-      <div style={{...arrowStyles, ...rightStyle}}/>
+      <div style={flexContainer}>
+        <div style={{...arrowStyles, ...leftStyle}}/>
+        <a href="google.com"><div style={smCircleStyle}></div></a>
+        <a href="google.com"><div style={lgCircleStyle}></div></a>
+        <a href="google.com"><div style={smCircleStyle}></div></a>
+        <div style={{...arrowStyles, ...rightStyle}}/>
+      </div>
     </div>
   )
 }
